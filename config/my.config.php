4aad6ff1751b784b4b1abb33397dd006e46d838f
stefanherdy <69188729+stefanherdy@users.noreply.github.com>
2020-10-30 08:22:45 +0100

    Update my.config.php

--- config/my.config.php
+++ config/my.config.php
@@ -1,1 +1,8 @@
-lorem ipsum
+<?php
+$CONFIG = array (
+  "trusted_proxies"   => ['127.0.0.1:8010'],
+  "overwritehost"     => "localhost:8010",
+  "overwriteprotocol" => "http",
+  "overwritewebroot"  => "/owncloud77",
+  "htaccess.RewriteBase" => "/owncloud77",
+);
